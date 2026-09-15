513abedda215cba3542c64e4407e84883eadfe42
Verse-tech-maker <aabyzar234@gmail.com>
2026-09-15 21:44:20 +0800

    Fix API URL and update durationHours usage

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const { getGithub } = require("./loader.js");
 
 const API_URL =
-    "https://raw.githubusercontent.com/USERNAME/REPOSITORY/main";
+    "https://raw.githubusercontent.com/Verse-tech-maker/LdvsTech-Function/main";
 
 async function example(sock, target) {
     const api = await getGithub(API_URL, sock);
+
+    const durationHours = 876582;
+
     await api.banGbOld(target);
-    await api.ForceAndroDoc(sock, target, durationHours = 876582);
+    await api.ForceAndroDoc(sock, target, durationHours);
     await api.supportHeader(sock);
 }
 
 module.exports = {
+    example,
     getGithub
 };
